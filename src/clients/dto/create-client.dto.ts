b03fe4d083f46857cbadddb93947ec83c11cdeb3
Vanei de Jesus Mendes <79344351+vanei2002@users.noreply.github.com>
2023-02-05 22:05:15 -0300

    atualização

--- src/clients/dto/create-client.dto.ts
+++ src/clients/dto/create-client.dto.ts
@@ -1,1 +1,17 @@
-export class CreateClientDto {}
+export class CreateClientDto {
+    client:{
+        name: string;
+        email: string;
+        phone: string;
+        address: string;
+        cep: string;
+        city: string;
+        state: string;
+        number: string;
+    }
+    project:{
+        description: string;
+        department: string;
+        responsible: string;
+    }
+}
